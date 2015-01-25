62c2e78c820c2b72ca3d9e530886b2285e3bcbec
Justin Giannone <jman.giannone@gmail.com>
2015-01-25 10:29:43 -0500

    Refactor Input System Only render valid Joystick Configs

--- GamepadListener.js
+++ GamepadListener.js
@@ -11,31 +11,23 @@ function GamepadListener(game) {
 
 	this.game = game;
 	this.gamepads = undefined;
-	this.leftSetupCanvases = [
-		document.createElement("canvas"),
-		document.createElement("canvas")
-	];
-	this.rightSetupCanvases = [
-		document.createElement("canvas"),
-		document.createElement("canvas"),
-		document.createElement("canvas")
-	];
+	this.leftSetupCanvases = [];
+	this.rightSetupCanvases = [];
 	this.setupPlayer = undefined;
 	
 	//Left Stick Config
-	for (canvasCounter = 0; canvasCounter < this.leftSetupCanvases.length; canvasCounter++) {
-		var canvas = this.leftSetupCanvases[canvasCounter];
-		canvas.width = canvas.height = this.configCanvasDimension;
-		canvas.addEventListener("click", this.handleJoystickConfigClick.bind(this, canvasCounter, "left"));
-		document.getElementById('leftStickCanvasContainer').appendChild(this.leftSetupCanvases[canvasCounter]);
+	for (canvasCounter = 0; canvasCounter < this.leftJoystickLayouts.length; canvasCounter++) {
+		var container = document.getElementById("leftStickCanvasContainer");
+		var canvas = this.createJoystickCanvas(container, canvasCounter, "left");
+		this.leftSetupCanvases[canvasCounter] = canvas;
 	}
 	
 	//Right Stick Config
-	for (canvasCounter = 0; canvasCounter < this.rightSetupCanvases.length; canvasCounter++) {
-		var canvas = this.rightSetupCanvases[canvasCounter];
-		canvas.width = canvas.height = this.configCanvasDimension;
-		canvas.addEventListener("click", this.handleJoystickConfigClick.bind(this, canvasCounter, "right"));
-		document.getElementById('rightStickCanvasContainer').appendChild(this.rightSetupCanvases[canvasCounter]);
+	for (canvasCounter = 0; canvasCounter < this.rightJoystickLayouts.length; canvasCounter++) {
+		var container = document.getElementById("rightStickCanvasContainer");
+		var canvas = this.createJoystickCanvas(container, canvasCounter, "right");
+		this.rightSetupCanvases[canvasCounter] = canvas;
+		
 	}
 	
 	Matter.Events.on(game.engine, 'tick', this.onTick.bind(this));
@@ -50,17 +42,17 @@ GamepadListener.prototype.onTick = function(tickEvent) {
 
 GamepadListener.prototype.handleJoystickConfigClick = function(index, side, mouseEvent) {	
 	if (side == "left") {
-		this.setupPlayer.gamepad.layout.leftJoystick = this.leftJoystickLayouts[index];
+		this.setupPlayer.gamepad.leftJoystick = this.leftJoystickLayouts[index];
 	}
 	else if (side == "right") {
-		this.setupPlayer.gamepad.layout.rightJoystick = this.rightJoystickLayouts[index];
+		this.setupPlayer.gamepad.rightJoystick = this.rightJoystickLayouts[index];
 	}
 	
 	if (this.setupPlayer.gamepad.setupComplete()) {
 		this.setupPlayer.addToWorld();
 		this.setupPlayer = undefined;
 	}
-}
+};
 
 GamepadListener.prototype.pollGamepads = function(tickEvent) {
 	var gamepads = navigator.getGamepads();
@@ -82,55 +74,55 @@ GamepadListener.prototype.pollGamepads = function(tickEvent) {
 			var input = gamepads[i];
 			player.gamepad.input = input;
 			
-			if (!player.gamepad.setupComplete() && (!this.setupPlayer || this.setupPlayer == player)) {
+			if (!player.gamepad.setupComplete() && (!this.setupPlayer || this.setupPlayer == player) && input) {
 				this.setupPlayer = player;
 				
 				for (layoutCounter = 0; layoutCounter < this.leftJoystickLayouts.length; layoutCounter++) {
 					var joystick = this.leftJoystickLayouts[layoutCounter];
 					var canvas = this.leftSetupCanvases[layoutCounter];
-					var context = canvas.getContext("2d");
-					var widthCenter = canvas.width / 2;
-					var heightCenter = canvas.height / 2;
-					context.clearRect(0, 0, canvas.width, canvas.height);
-					context.beginPath();
-					context.arc(widthCenter, heightCenter, widthCenter, 2 * Math.PI, 0, true);
-					context.stroke();
-					context.beginPath();
-					context.arc(widthCenter + (input.axes[joystick.horizontalAxis] * this.joystickIndicatorRange), heightCenter + (input.axes[joystick.verticalAxis] * this.joystickIndicatorRange), widthCenter / 4, 2 * Math.PI, 0, true);
-					context.strokeStyle = "white";
-					context.stroke();
+					this.setupJoystickCanvas(joystick, input, canvas);
 				}
 				
 				for (layoutCounter = 0; layoutCounter < this.rightJoystickLayouts.length; layoutCounter++) {
 					var joystick = this.rightJoystickLayouts[layoutCounter];
 					var canvas = this.rightSetupCanvases[layoutCounter];
-					var context = canvas.getContext("2d");
-					var widthCenter = canvas.width / 2;
-					var heightCenter = canvas.height / 2;
-					context.clearRect(0, 0, canvas.width, canvas.height);
-					context.beginPath();
-					context.arc(widthCenter, heightCenter, widthCenter, 2 * Math.PI, 0, true);
-					context.stroke();
-					context.beginPath();
-					context.arc(widthCenter + (input.axes[joystick.horizontalAxis] * this.joystickIndicatorRange), heightCenter + (input.axes[joystick.verticalAxis] * this.joystickIndicatorRange), widthCenter / 4, 2 * Math.PI, 0, true);
-					context.strokeStyle = "white";
-					context.stroke();
+					this.setupJoystickCanvas(joystick, input, canvas);
 				}
 				
 				document.getElementById("gamepadCanvasContainer").style.display = "block";
 			}
 			else if (!this.setupPlayer) {
 				document.getElementById("gamepadCanvasContainer").style.display = "none";
-			
-				this.leftSetupCanvases.forEach(function(canvas) {
-					var context = canvas.getContext("2d");
-					context.clearRect(0, 0, canvas.width, canvas.height);
-				});
-				this.rightSetupCanvases.forEach(function(canvas) {
-					var context = canvas.getContext("2d");
-					context.clearRect(0, 0, canvas.width, canvas.height);
-				});
 			}
 		}
+	}
+};
+
+GamepadListener.prototype.createJoystickCanvas = function(container, canvasIndex, side) {
+	var canvas = document.createElement("canvas");
+	canvas.width = canvas.height = this.configCanvasDimension;
+	canvas.addEventListener("click", this.handleJoystickConfigClick.bind(this, canvasIndex, side));
+	container.appendChild(canvas);
+	
+	return canvas;
+};
+
+GamepadListener.prototype.setupJoystickCanvas = function(joystick, input, canvas) {
+	if(joystick.horizontalAxis < input.axes.length && joystick.verticalAxis < input.axes.length){
+		var context = canvas.getContext("2d");
+		var widthCenter = canvas.width / 2;
+		var heightCenter = canvas.height / 2;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.beginPath();
+		context.arc(widthCenter, heightCenter, widthCenter, 2 * Math.PI, 0, true);
+		context.stroke();
+		context.beginPath();
+		context.arc(widthCenter + (joystick.getHorizontalAxis(input) * this.joystickIndicatorRange), heightCenter + (joystick.getVerticalAxis(input) * this.joystickIndicatorRange), widthCenter / 4, 2 * Math.PI, 0, true);
+		context.strokeStyle = "white";
+		context.stroke();
+		canvas.style.display = "";
+	}
+	else {
+		canvas.style.display = "none";
 	}
 };
--- Joystick.js
+++ Joystick.js
@@ -2,3 +2,25 @@ function Joystick(horizontalAxis, verticalAxis) {
 	this.horizontalAxis = horizontalAxis;
 	this.verticalAxis = verticalAxis;
 }
+
+Joystick.prototype.deadZone = 0.2;
+
+Joystick.prototype.getHorizontalAxis = function(input) {
+	var axis = input.axes[this.horizontalAxis];
+	
+	if(Math.abs(axis) < this.deadZone) {
+		axis = 0;
+	}
+	
+	return axis;
+};
+
+Joystick.prototype.getVerticalAxis = function(input) {
+	var axis = input.axes[this.verticalAxis];
+	
+	if(Math.abs(axis) < this.deadZone) {
+		axis = 0;
+	}
+	
+	return axis;
+};
